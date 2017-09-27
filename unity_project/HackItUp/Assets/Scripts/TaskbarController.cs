964f796c89945b0c4e3787bfb89f85eeb89d6ac3
fuchs- <fernando.bueno.lima@gmail.com>
2017-09-26 22:24:14 -0300

    Added a bunch of stuff
    -Added main scene
    -Added wallpaper
    -Added Taskbar basic structure

---
+++ unity_project/HackItUp/Assets/Scripts/TaskbarController.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class TaskbarController : MonoBehaviour
+{
+    public void BtnStartMenuPressed()
+    {
+        Debug.Log("Start Menu Button was pressed");
+    }
+
+    public void BtnConsolePressed()
+    {
+        Debug.Log("Console Button was pressed");
+    }
+}
